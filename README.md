843ee481cdeaf3d8f709e6eae88386fb6f78a984
petermchale <peter.thomas.mchale@gmail.com>
2021-06-10 17:14:32 -0600

    data gotchas; icgc download

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # constraint-tools
+
+Mutation Annotation Format (MAF) specification: https://docs.gdc.cancer.gov/Data/File_Formats/MAF_Format/
+
+
+## Gotchas
+
+Based upon: https://www.biostars.org/p/69222/
+
+1. Ensure that different tumor sample barcodes were not used for the same sample by reducing the tumor IDs to the form "TCGA-XX-XXXX-XX". 
+2. Our model assumes that the probability of generating a mutation at a given site in a given tumor is independent of whether a mutation has been seen at the same site in another tumor. That assumption is violated when tumors share a common ancestor, as is the case for a primary tumor and a metastasis. Therefore one would probably want to ensure that no more than one tumor per patient is included in the maf.
+
+ 
---
+++ download_icgc.sh
@@ -0,0 +1,30 @@
+set -o errexit
+set -o pipefail
+# set -o noclobber
+
+set -o xtrace
+# Must use single quote to prevent variable expansion.
+# For example, if double quotes were used, ${LINENO} would take on the value of the current line,
+# instead of its value when PS4 is used later in the script
+# https://stackoverflow.com/a/6697845/6674256
+# ${FOO:+val}    val if $FOO is set
+# ${FOO[0]}   element #0 of the FOO array
+# https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
+PS4='+ (${BASH_SOURCE[0]##*/} @ ${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+export RED='\033[0;31m'
+export CYAN='\033[0;36m'
+export NO_COLOR='\033[0m' 
+
+echo -e "${CYAN}Downloading and indexing ICGC mutations...${NO_COLOR}\n"
+
+# https://dcc.icgc.org/releases/PCAWG/consensus_snv_indel/
+# Supplementary Table 4 at https://www.nature.com/articles/s41586-020-1969-6#MOESM3
+url="https://dcc.icgc.org/api/v1/download?fn=/PCAWG/consensus_snv_indel/final_consensus_passonly.snv_mnv_indel.icgc.public.maf.gz"
+path="data/icgc/mutations"
+
+# wget ${url} --output-document=${path}.maf.gz 
+# gunzip ${path}.maf.gz
+# bgzip ${path}.maf
+tabix ${path}.maf.gz
+
