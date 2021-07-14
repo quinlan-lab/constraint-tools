8259637681a943ea9eb7a899ea715242665e6805
u1240855 <u1240855@utah.edu>
2021-07-14 13:03:36 -0600

    script to download gap file

---
+++ download/download_gaps.sh
@@ -0,0 +1,44 @@
+
+set -o errexit
+set -o pipefail
+# set -o noclobber
+
+set -o xtrace
+# Must use single quote to prevent variable expansion.
+# For example, if double quotes were used, ${LINENO} would take on the value of the current line,
+# instead of its value when PS4 is used later in the script
+# https://stackoverflow.com/a/6697845/6674256
+# ${FOO:+val}    val if $FOO is set
+# ${FOO[0]}   element #0 of the FOO array
+# https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
+PS4='+ (${BASH_SOURCE[0]##*/} @ ${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+export RED='\033[0;31m'
+export CYAN='\033[0;36m'
+export NO_COLOR='\033[0m' 
+
+# gap file: ftp://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/gap.txt.gz
+# gap file contains telomere and centromere information
+
+
+url = "ftp://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/gap.txt.gz"
+root = "/scratch/ucgd/lustre-work/quinlan/u1240855/constraint-tools"
+path = "${root}/data/gap"
+
+mkdir --parents ${path}
+
+download_file () {
+    local url=$1
+    wget ${url} --output-document=${path}/gap.txt.gz
+}
+
+echo -e "${CYAN}Downloading UCSC gap file...${NO_COLOR}\n"
+download_file ${gap_url}
+
+echo -e "${CYAN}Decompressing UCSC gap file...${NO_COLOR}\n"
+set +o errexit
+gzip --decompress ${path}/gap.txt.gz
+set -o errexit
+
+
+
