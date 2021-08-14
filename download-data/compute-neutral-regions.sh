58ac437c26ae1c1a77e2f6c23a189740f005db5b
petermchale <peter.thomas.mchale@gmail.com>
2021-08-13 18:48:04 -0600

    real putatively neutral training regions

--- download-data/compute-neutral-regions.sh
+++ download-data/compute-neutral-regions.sh
@@ -12,6 +12,7 @@ gaps="${CONSTRAINT_TOOLS}/data/gaps/gaps.sorted.bed.gz"
 encode_exclude_regions="${CONSTRAINT_TOOLS}/data/encode-exclude-regions/encode-exclude-regions.sorted.bed.gz"
 
 info "Compute regions putatively under neutral selection..."
+info "This is the strategy Kelley Harris’s lab took" 
 set -o xtrace
 bedtools complement -i ${exons} -g ${chromosome_sizes} \
   | bedtools intersect -v -a - -b ${gaps} ${encode_exclude_regions} \
--- download-data/download-encode-exclude-regions.sh
+++ download-data/download-encode-exclude-regions.sh
@@ -6,6 +6,7 @@ set -o nounset
 
 source download-data/set-environment-variables.sh 
 
+# https://www.nature.com/articles/s41598-019-45839-z
 # https://www.encodeproject.org/files/ENCFF001TDO/
 encode_exclude_regions_url="https://www.encodeproject.org/files/ENCFF001TDO/@@download/ENCFF001TDO.bed.gz" 
 encode_exclude_regions="encode-exclude-regions.sorted"
