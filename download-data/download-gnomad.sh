2b3d644aebd5bfa1e4e0507599539e19244459c8
u1240855 <u1240855@utah.edu>
2021-08-23 10:15:06 -0600

    code to filter coverage file

---
+++ download-data/download-gnomad.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#!/bin/bash
+#SBATCH --time=3:00:00
+#SBATCH --nodes=2
+#SBATCH --ntasks=16
+#SBATCH --account=quinlan-rw
+#SBATCH --partition=quinlan-shared-rw
+
+set -o errexit
+set -o pipefail
+# set -o noclobber
+set -o xtrace
+set -o nounset 
+
+source download-data/set-environment-variables.sh 
+
+
+## Define input variables
+while getopts c:t: flag
+do
+	case "${flag}" in
+		c) column=${OPTARG};;
+		t) threshold=${OPTARG};;
+	esac
+done
+
+## Define files to download
+gnomad_url="https://storage.googleapis.com/gcp-public-data--gnomad/release/3.1.1/vcf/genomes/gnomad.genomes.v3.1.1.sites.chr22.vcf.bgz"
+gnomad_tbi_url="https://storage.googleapis.com/gcp-public-data--gnomad/release/3.1.1/vcf/genomes/gnomad.genomes.v3.1.1.sites.chr22.vcf.bgz.tbi"
+gnomad_coverage_v3="https://storage.googleapis.com/gcp-public-data--gnomad/release/3.0.1/coverage/genomes/gnomad.genomes.r3.0.1.coverage.summary.tsv.bgz"
+
+## Define directory to download files into 
+gnomad_path="${CONSTRAINT_TOOLS}/data/gnomad/v3"
+
+## Define output names 
+gnomad_variant="gnomad_v3_chr22.vcf.bgz"
+gnomad_tbi="gnomad_v3_chr22.vcf.bgz.tbi"
+gnomad_coverage="gnomad_v3_coverage.summary.tsv.bgz"
+
+mkdir --parents ${gnomad_path}
+
+info "Downloading gnomad's variant VCF file..."
+#wget ${gnomad_url} --output-document=${gnomad_path}/${gnomad_variant}
+
+info "Downloading gnomad's tbi file..."
+#wget ${gnomad_tbi_url} --output-document=${gnomad_path}/${gnomad_tbi}
+
+info "Downloading gnomad v3 coverage file..."
+#wget ${gnomad_coverage_v3} --output-document=${gnomad_path}/${gnomad_coverage}
+
+info "Filtering gnomad v3 coverage file..."
+## Define coverage threshold
+gnomad_coverage_filtered="gnomad_v3_coverage_${threshold}.bed"
+
+## Filter coverage file 
+zcat ${gnomad_path}/${gnomad_coverage} --force | tail -n+2 | awk -v c=${column} -v t=${threshold} '$c>t {print $1}' | sed 's/:/\t/g' | awk '{print $1"\t"($2-1)"\t"$2}' | bedtools merge > ${gnomad_path}/${gnomad_coverage_filtered}
+
+
