cbc72940ffbcd244043f9651760cf1b71e6b7faf
u1240855 <u1240855@utah.edu>
2021-09-02 10:56:51 -0600

    intersect with sufficiently covered regions

--- download-data/compute-neutral-regions.sh
+++ download-data/compute-neutral-regions.sh
@@ -11,9 +11,13 @@ chromosome_sizes="${CONSTRAINT_TOOLS}/data/chromosome-sizes/hg19.chrom.sizes.sor
 gaps="${CONSTRAINT_TOOLS}/data/gaps/gaps.sorted.bed.gz"
 encode_exclude_regions="${CONSTRAINT_TOOLS}/data/encode-exclude-regions/encode-exclude-regions.sorted.bed.gz"
 
+covered_sites="${CONSTRAINT_TOOLS}/data/gnomad/v3/coverage/gnomad_v3_coverage.filtered.sorted.bed.gz"
+
+
 info "Compute regions putatively under neutral selection..."
 info "This is the strategy Kelley Harris’s lab took" 
 set -o xtrace
 bedtools complement -i ${exons} -g ${chromosome_sizes} \
   | bedtools intersect -v -a - -b ${gaps} ${encode_exclude_regions} \
+  | bedtools intersect -a - -b ${covered_sites} \
   | sort-compress-index-bed --name ${CONSTRAINT_TOOLS}/dist/neutral-regions
