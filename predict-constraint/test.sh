601d0146672c3680c2d3035bf2c26e829d9ecf0e
petermchale <peter.thomas.mchale@gmail.com>
2021-10-21 12:11:53 -0600

    x

---
+++ predict-constraint/test.sh
@@ -0,0 +1,9 @@
+# https://stackoverflow.com/a/43476575/6674256
+# need to export PYTHONPATH since it is not already in the environment: 
+# `printenv | grep -w PYTHONPATH` returns zero output
+
+CONSTRAINT_TOOLS=$1
+
+export PYTHONPATH="${CONSTRAINT_TOOLS}/utilities:${CONSTRAINT_TOOLS}/predict-constraint"
+
+python predict-constraint/compute_mutation_counts.py 
