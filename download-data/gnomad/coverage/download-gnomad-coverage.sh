0a6d0e0a86217b7c1168c559a153923ecdf6dbb4
petermchale <peter.thomas.mchale@gmail.com>
2021-12-10 12:29:39 -0700

    condense and make explicit code to download and filter gnomad coverage

--- download-data/gnomad/coverage/download-gnomad-coverage.sh
+++ download-data/gnomad/coverage/download-gnomad-coverage.sh
@@ -24,7 +24,7 @@ set -o nounset
 
 source download-data/set-environment-variables.sh 
 
-info "Filterting on fraction-threshold value of: ${fraction_threshold}..."
+info "Filtering on fraction-threshold value of: ${fraction_threshold}..."
 info "Filtering for sites with 10x coverage..."
 info "Filtering gnomad-${version} ${seq} coverage file to select for positions in which (${fraction_threshold} * 100)% of samples have a mean depth of value: 10x"
 
@@ -61,7 +61,7 @@ elif [ ${seq} == "wgs" ] && [ ${version} == "v2" ]; then
 	#wget ${url} --output-document=${coverage_path}/${gnomad_coverage_file}
 
 	info "Performing the above filter..."
-        ## coverage column 7 for over 10x --> coverage_threshold=7
+  ## coverage column 7 for over 10x --> coverage_threshold=7
 	zcat ${coverage_path}/${gnomad_coverage_file} | tail -n+2 | awk -v c=7 -v t=${fraction_threshold} '$c>t {print "chr"$1"\t"($2-1)"\t"$2}' | bedtools merge > ${coverage_path}/${gnomad_coverage_filtered}.hg19.bed
 
 	## https://gist.github.com/brentp/894555#file-lift-sh
@@ -73,28 +73,28 @@ elif [ ${seq} == "wgs" ] && [ ${version} == "v2" ]; then
 	rm ${coverage_path}/${gnomad_coverage_filtered}.hg19.bed
 
 elif [ ${seq} == "wes" ] && [ ${version} == "v2" ]; then
-        url="https://storage.googleapis.com/gcp-public-data--gnomad/release/2.1/coverage/exomes/gnomad.exomes.coverage.summary.tsv.bgz"
-        gnomad_coverage_file="gnomad_v2_wes_coverage.summary.tsv.bgz"
-        gnomad_coverage_filtered="gnomad_v2_wes_coverage.filtered"
+  url="https://storage.googleapis.com/gcp-public-data--gnomad/release/2.1/coverage/exomes/gnomad.exomes.coverage.summary.tsv.bgz"
+  gnomad_coverage_file="gnomad_v2_wes_coverage.summary.tsv.bgz"
+  gnomad_coverage_filtered="gnomad_v2_wes_coverage.filtered"
 
 	## Define directory to download files into
 	coverage_path="${CONSTRAINT_TOOLS}/data/gnomad/v2/wes/coverage"
 	mkdir --parents ${coverage_path}
 	
-        info "Downloading gnomad v2 wes coverage file"
-        #wget ${url} --output-document=${coverage_path}/${gnomad_coverage_file}
+  info "Downloading gnomad v2 wes coverage file"
+  #wget ${url} --output-document=${coverage_path}/${gnomad_coverage_file}
 	
 	info "Performing the above filter..."
-        ## coverage column 7 for over 10x --> coverage_threshold=7
-        zcat ${coverage_path}/${gnomad_coverage_file} | tail -n+2 | awk -v c=7 -v t=${fraction_threshold} '$c>t {print "chr"$1"\t"($2-1)"\t"$2}' | bedtools merge > ${coverage_path}/${gnomad_coverage_filtered}.hg19.bed	
+  ## coverage column 7 for over 10x --> coverage_threshold=7
+  zcat ${coverage_path}/${gnomad_coverage_file} | tail -n+2 | awk -v c=7 -v t=${fraction_threshold} '$c>t {print "chr"$1"\t"($2-1)"\t"$2}' | bedtools merge > ${coverage_path}/${gnomad_coverage_filtered}.hg19.bed	
 
 	## https://gist.github.com/brentp/894555#file-lift-sh
 	info "Perform liftover to convert hg19 coordinates to hg38"
-        bash ${CONSTRAINT_TOOLS}/download-data/lift.sh ${coverage_path}/${gnomad_coverage_filtered}.hg19.bed hg19 hg38 
+  bash ${CONSTRAINT_TOOLS}/download-data/lift.sh ${coverage_path}/${gnomad_coverage_filtered}.hg19.bed hg19 hg38 
 
-        ## Remove intermediate files
-        mv ${coverage_path}/${gnomad_coverage_filtered}.hg19.bed.hg38 ${coverage_path}/${gnomad_coverage_filtered}.hg38.bed
-        rm ${coverage_path}/${gnomad_coverage_filtered}.hg19.bed
+  ## Remove intermediate files
+  mv ${coverage_path}/${gnomad_coverage_filtered}.hg19.bed.hg38 ${coverage_path}/${gnomad_coverage_filtered}.hg38.bed
+  rm ${coverage_path}/${gnomad_coverage_filtered}.hg19.bed
 else 
 	echo "input valid gnomad version number and sequencing strategy employed..."
 	exit 1
