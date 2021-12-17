2e36a9025c720713d15e8cdb5a845a825d5bbc0f
petermchale <peter.thomas.mchale@gmail.com>
2021-12-16 17:36:20 -0700

    distribute neutral regions for the germline case and grch38

--- download-data/compute-neutral-regions.sh
+++ download-data/compute-neutral-regions.sh
@@ -5,13 +5,13 @@ set -o nounset
 
 source download-data/set-environment-variables.sh 
 
-exons="${CONSTRAINT_TOOLS}/data/genes/exons.sorted.bed.gz"
-chromosome_sizes="${CONSTRAINT_TOOLS}/data/chromosome-sizes/hg19.chrom.sizes.sorted"
+exons="${CONSTRAINT_TOOLS_DATA}/genes/grch38/exons.sorted.bed.gz"
+chromosome_sizes="${CONSTRAINT_TOOLS_DATA}/reference/grch38/chromosome-sizes/hg38.chrom.sizes.sorted"
 
-gaps="${CONSTRAINT_TOOLS}/data/gaps/gaps.sorted.bed.gz"
-encode_exclude_regions="${CONSTRAINT_TOOLS}/data/encode-exclude-regions/encode-exclude-regions.sorted.bed.gz"
+gaps="${CONSTRAINT_TOOLS_DATA}/gaps/grch38/gaps.sorted.bed.gz"
+encode_exclude_regions="${CONSTRAINT_TOOLS_DATA}/encode-exclude-regions/grch38/encode-exclude-regions.sorted.bed.gz"
 
-covered_sites="${CONSTRAINT_TOOLS}/data/gnomad/v3/coverage/gnomad_v3_coverage.filtered.sorted.bed.gz"
+covered_sites="${CONSTRAINT_TOOLS_DATA}/gnomad/v3/gnomad_v3_coverage.filtered.sorted.bed.gz"
 
 info "Compute regions putatively under neutral selection..."
 info "This is the strategy Kelley Harris’s lab took" 
@@ -19,4 +19,4 @@ set -o xtrace
 bedtools complement -i ${exons} -g ${chromosome_sizes} \
   | bedtools intersect -v -a - -b ${gaps} ${encode_exclude_regions} \
   | bedtools intersect -a - -b ${covered_sites} \
-  | sort-compress-index-bed --name ${CONSTRAINT_TOOLS}/dist/neutral-regions
+  | sort-compress-index-bed --name ${CONSTRAINT_TOOLS}/dist/neutral-regions-germline-grch38
