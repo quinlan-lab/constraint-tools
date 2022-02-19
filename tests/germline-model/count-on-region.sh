92fb58b65d28a641ffd7dd00f6ccfdf8331ba9db
petermchale <peter.thomas.mchale@gmail.com>
2022-02-18 17:44:03 -0700

    first draft of code to train germline model (minus aggregation of counts over neutral regions)

---
+++ tests/germline-model/count-on-region.sh
@@ -0,0 +1,33 @@
+set -o errexit
+set -o pipefail
+set -o noclobber
+set -o nounset
+
+mutations="/scratch/ucgd/lustre-work/quinlan/data-shared/constraint-tools/gnomad/v3/variants/gnomad_v3.sorted.tsv.gz"
+genome="/scratch/ucgd/lustre-work/quinlan/data-shared/constraint-tools/reference/grch38/hg38.analysisSet.fa.gz"
+neutral_region="chr1:15363-15768"
+number_chromosomes_min="150000"
+kmer_size="3" 
+tmpdir="/scratch/ucgd/lustre-work/quinlan/u6018199/constraint-tools/tests/germline-model/tmpdir"
+window_size="51" 
+
+mkdir --parents ${tmpdir}
+
+source set-environment-variables.sh 
+
+PATH="${CONSTRAINT_TOOLS}/train/germline-model:$PATH" 
+
+# https://stackoverflow.com/a/43476575/6674256
+# need to export PYTHONPATH since it is not already in the environment: 
+# `printenv | grep -w PYTHONPATH` returns zero output
+export PYTHONPATH="${CONSTRAINT_TOOLS}/utilities"
+
+count-on-region \
+  --genome ${genome} \
+  --mutations ${mutations} \
+  --number-chromosomes-min ${number_chromosomes_min} \
+  --kmer-size ${kmer_size} \
+  --tmpdir ${tmpdir} \
+  --neutral-region ${neutral_region} \
+  --window-size ${window_size}
+
---
+++ train-germline-model.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#SBATCH --time=3:00:00
+#SBATCH --account=quinlan-rw
+#SBATCH --partition=quinlan-shared-rw
+#SBATCH --output=dist/train-germline-model.log
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o noclobber
+# set -o xtrace
+
+CONSTRAINT_TOOLS=$1 
+CONSTRAINT_TOOLS_DATA="/scratch/ucgd/lustre-work/quinlan/data-shared/constraint-tools" 
+
+PATH=${CONSTRAINT_TOOLS}:$PATH 
+PATH=${CONSTRAINT_TOOLS}/utilities:$PATH 
+PATH=${CONSTRAINT_TOOLS}/bin:$PATH 
+
+genome="/scratch/ucgd/lustre-work/quinlan/data-shared/constraint-tools/reference/grch38/hg38.analysisSet.fa.gz"
+mutations="/scratch/ucgd/lustre-work/quinlan/data-shared/constraint-tools/gnomad/v3/variants/gnomad_v3.sorted.tsv.gz"
+number_chromosomes_min="120000"
+kmer_size="3"
+neutral_regions="${CONSTRAINT_TOOLS}/dist/neutral-regions-germline-grch38.bed.gz"
+model="${CONSTRAINT_TOOLS}/dist" # path to directory to store model in
+work="${CONSTRAINT_TOOLS_DATA}/work/train-germline-model" # path to directory to store intermediate work and logs
+mkdir --parents ${work}
+
+fetch_subset_of_neutral_regions () { 
+  less ${neutral_regions} | head -100 
+}
+
+train_on_subset_of_neutral_regions () {
+  info "$(fetch_subset_of_neutral_regions | awk '{ print $0, $3-$2 }')"
+
+  constraint-tools train-germline-model \
+    --genome ${genome} \
+    --mutations ${mutations} \
+    --number-chromosomes-min ${number_chromosomes_min} \
+    --kmer-size ${kmer_size} \
+    --neutral-regions <(fetch_subset_of_neutral_regions | bgzip) \
+    --model ${model} \
+    --work ${work}
+}
+
+train_on_all_regions () {
+  constraint-tools train-germline-model \
+    --genome ${genome} \
+    --mutations ${mutations} \
+    --number-chromosomes-min ${number_chromosomes_min} \
+    --kmer-size ${kmer_size} \
+    --neutral-regions ${neutral_regions} \
+    --model ${model} \
+    --work ${work}
+}
+
+train_on_subset_of_regions
+# train_on_all_regions
