ac8425541aa186b0ec3663e060dc3d3561acf5a0
petermchale <peter.thomas.mchale@gmail.com>
2022-02-23 18:11:06 -0700

    test aggregate-counts

---
+++ tests/germline-model/aggregate-counts.sh
@@ -0,0 +1,31 @@
+set -o errexit
+set -o pipefail
+set -o noclobber
+set -o nounset
+
+tmpdir="/scratch/ucgd/lustre-work/quinlan/u6018199/constraint-tools/tests/germline-model/tmpdir"
+model="/scratch/ucgd/lustre-work/quinlan/u6018199/constraint-tools/tests/germline-model/model-germline-grch38.json"
+number_of_neutral_regions="2"
+kmer_size="3"
+
+mkdir --parents ${tmpdir}
+
+source set-environment-variables.sh 
+
+PATH="${CONSTRAINT_TOOLS}/train/germline-model:$PATH" 
+
+# https://stackoverflow.com/a/43476575/6674256
+# need to export PYTHONPATH since it is not already in the environment: 
+# `printenv | grep -w PYTHONPATH` returns zero output
+export PYTHONPATH="${CONSTRAINT_TOOLS}/utilities"
+
+aggregate-counts \
+  --genome "XXX" \
+  --mutations "XXX" \
+  --number-chromosomes-min "-1" \
+  --kmer-size ${kmer_size} \
+  --tmpdir ${tmpdir} \
+  --window-size "-1" \
+  --model ${model} \
+  --number-of-neutral-regions ${number_of_neutral_regions} \
+  --neutral-regions "XXX"
