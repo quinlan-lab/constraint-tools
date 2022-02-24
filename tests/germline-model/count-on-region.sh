82523cd2f9e739f05f0de299042419aecd87471d
petermchale <peter.thomas.mchale@gmail.com>
2022-02-23 18:15:37 -0700

    test count-on-region using two neutral regions

--- tests/germline-model/count-on-region.sh
+++ tests/germline-model/count-on-region.sh
@@ -5,7 +5,6 @@ set -o nounset
 
 mutations="/scratch/ucgd/lustre-work/quinlan/data-shared/constraint-tools/gnomad/v3/variants/gnomad_v3.sorted.tsv.gz"
 genome="/scratch/ucgd/lustre-work/quinlan/data-shared/constraint-tools/reference/grch38/hg38.analysisSet.fa.gz"
-neutral_region="chr1:15363-15768"
 number_chromosomes_min="140000"
 kmer_size="3" 
 tmpdir="/scratch/ucgd/lustre-work/quinlan/u6018199/constraint-tools/tests/germline-model/tmpdir"
@@ -22,12 +21,13 @@ PATH="${CONSTRAINT_TOOLS}/train/germline-model:$PATH"
 # `printenv | grep -w PYTHONPATH` returns zero output
 export PYTHONPATH="${CONSTRAINT_TOOLS}/utilities"
 
-count-on-region \
-  --genome ${genome} \
-  --mutations ${mutations} \
-  --number-chromosomes-min ${number_chromosomes_min} \
-  --kmer-size ${kmer_size} \
-  --tmpdir ${tmpdir} \
-  --neutral-region ${neutral_region} \
-  --window-size ${window_size}
-
+for neutral_region in "chr1:15363-15768" "chr1:102847-103286"; do
+  count-on-region \
+    --genome ${genome} \
+    --mutations ${mutations} \
+    --number-chromosomes-min ${number_chromosomes_min} \
+    --kmer-size ${kmer_size} \
+    --tmpdir ${tmpdir} \
+    --neutral-region ${neutral_region} \
+    --window-size ${window_size}
+done
