bab3210a7c1b632780b7df457df750254b9a0082
petermchale <peter.thomas.mchale@gmail.com>
2022-03-03 16:49:03 -0700

    test/benchmark train-germline-model.sh

---
+++ tests/germline-model/train-germline-model.sh
@@ -0,0 +1,7 @@
+
+bash train-germline-model.sh \
+  --constraint-tools-directory "$PWD" \
+  --number-of-neutral-regions "100" \
+  --work-subdirectory "work-test" \
+  --work-directory-should-be-clean "false"
+
