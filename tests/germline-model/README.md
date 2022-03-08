84d351c84bffa897b29c55380b1e4c72c1e504e5
petermchale <peter.thomas.mchale@gmail.com>
2022-03-08 10:41:34 -0700

    test aggregate_kmer_counts function

---
+++ tests/germline-model/README.md
@@ -0,0 +1,5 @@
+## TODO 
+
+Tests currently generate output that a human must vet. 
+Automate this by recording the output that should be generated as "expected output", 
+and then "assert" that actual output from tests equals the expected output. 
--- utilities/aggregate_counts.py
+++ utilities/aggregate_counts.py
@@ -23,6 +23,64 @@ def aggregate_counts(fetch_counts, args, kmerCounts_log, singletonCounts_log):
   singleton_counts = aggregate_singleton_counts(fetch_counts, args, singletonCounts_log)
   return kmer_counts, singleton_counts
 
+def fetch_subdict(d): 
+  return {
+    k: d[k]
+    for k in ['AAA', 'AAC', 'AGA']
+  }
+
+class Args: 
+  kmer_size = None
+  
+def fetch_args(): 
+  args = Args()
+  args.kmer_size = 3 
+  return args
+
+def test_aggregate_kmer_counts(): 
+  from colorize import print_unbuffered, print_json
+  print_unbuffered('')
+  print_string_as_info('********* testing aggregate_kmer_counts function: ****************')
+  print_unbuffered('')
+
+  from kmer import initialize_kmer_counts_germline
+
+  # https://realpython.com/introduction-to-python-generators/
+  def fetch_counts(count_type, args, progress_bar):
+    if count_type != 'kmerCounts': 
+      raise ValueError
+
+    x = initialize_kmer_counts_germline(args)
+    x['AAA']['count'] += 1
+    x['AAA']['count'] += 1
+    x['AAA']['ALTStateCounts']['{C,T}'] += 1
+    x['AAC']['count'] += 1
+    x['AGA']['count'] += 1
+    x['AGA']['ALTStateCounts']['{A}'] += 1
+    print('x:')
+    print_json(fetch_subdict(x))
+    yield x 
+
+    y = initialize_kmer_counts_germline(args)
+    y['AAA']['count'] += 1
+    y['AAA']['ALTStateCounts']['{C,T}'] += 1
+    y['AGA']['count'] += 1
+    y['AGA']['ALTStateCounts']['{C}'] += 1
+    print('y:')
+    print_json(fetch_subdict(y))
+    yield y
+
+    z = initialize_kmer_counts_germline(args)
+    z['AGA']['count'] += 1
+    z['AGA']['ALTStateCounts']['{C}'] += 1
+    print('z:')
+    print_json(fetch_subdict(z))
+    yield z
+
+  kmer_counts = aggregate_kmer_counts(fetch_counts, args=fetch_args(), progress_bar=None)
+  print('aggregate x, y and z:') 
+  print_json(fetch_subdict(kmer_counts))
+
 def test_aggregate_singleton_counts(): 
   from colorize import print_unbuffered
   print_unbuffered('')
@@ -59,8 +117,9 @@ def test_aggregate_singleton_counts():
     yield z 
 
   singleton_counts = aggregate_singleton_counts(fetch_counts, args=None, progress_bar=None)
-  print('sum of x, y and z:', singleton_counts)
+  print('aggregate x, y and z:', singleton_counts)
 
 if __name__ == '__main__': 
+  test_aggregate_kmer_counts()
   test_aggregate_singleton_counts() 
 
