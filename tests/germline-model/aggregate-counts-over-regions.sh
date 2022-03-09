7ad947780b6d8fe9972df95fd266a8ad5136abb2
petermchale <peter.thomas.mchale@gmail.com>
2022-03-08 17:06:23 -0700

    aggregate (in memory) kmer and singleton counts over a batch of regions assigned to a single slurm job

---
+++ tests/germline-model/aggregate-counts-over-regions.sh
@@ -0,0 +1,42 @@
+set -o errexit
+set -o pipefail
+set -o noclobber
+set -o nounset
+
+mutations="/scratch/ucgd/lustre-work/quinlan/data-shared/constraint-tools/gnomad/v3/variants/gnomad_v3.sorted.tsv.gz"
+genome="/scratch/ucgd/lustre-work/quinlan/data-shared/constraint-tools/reference/grch38/hg38.analysisSet.fa.gz"
+number_chromosomes_min="140000"
+kmer_size="3" 
+tmpdir="/scratch/ucgd/lustre-work/quinlan/u6018199/constraint-tools/tests/germline-model/tmpdir"
+window_size="51" 
+progress_bar="stdout" # "stdout" or a directory to log progress to
+
+mkdir --parents ${tmpdir}
+
+source set-environment-variables.sh 
+
+PATH="${CONSTRAINT_TOOLS}/train/germline-model:$PATH" 
+
+# https://stackoverflow.com/a/43476575/6674256
+# need to export PYTHONPATH since it is not already in the environment: 
+# `printenv | grep -w PYTHONPATH` returns zero output
+export PYTHONPATH="${CONSTRAINT_TOOLS}/utilities"
+
+line_number_of_first_region="1"
+line_number_of_last_region="7"
+neutral_regions_for_job="${tmpdir}/neutral-regions.${line_number_of_first_region}-${line_number_of_last_region}.bed"
+counts_for_job="${tmpdir}/counts.${line_number_of_first_region}-${line_number_of_last_region}.json"
+
+echo "" 
+info "Performing counts on:" "${neutral_regions_for_job}"
+info "Writing counts to:" "${counts_for_job}"
+echo "" 
+aggregate-counts-over-regions \
+  --genome ${genome} \
+  --mutations ${mutations} \
+  --number-chromosomes-min ${number_chromosomes_min} \
+  --kmer-size ${kmer_size} \
+  --neutral-regions-filename ${neutral_regions_for_job} \
+  --counts-filename ${counts_for_job} \
+  --window-size ${window_size} \
+  --progress-bar ${progress_bar} 
