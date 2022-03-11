1cfcfc214c4f1b8ed7605d11a1a07e99aa59c119
petermchale <peter.thomas.mchale@gmail.com>
2022-03-11 14:00:47 -0700

    benchmark training durations using a kmer of 5 (instead of 3)

--- train-germline-model.sh
+++ train-germline-model.sh
@@ -37,9 +37,11 @@ PATH=${CONSTRAINT_TOOLS}/bin:$PATH
 genome="/scratch/ucgd/lustre-work/quinlan/data-shared/constraint-tools/reference/grch38/hg38.analysisSet.fa.gz"
 mutations="/scratch/ucgd/lustre-work/quinlan/data-shared/constraint-tools/gnomad/v3/variants/gnomad_v3.sorted.tsv.gz"
 number_chromosomes_min="130000"
-kmer_size="3"
-neutral_regions="${CONSTRAINT_TOOLS}/dist/neutral-regions-germline-grch38.bed.gz"
+kmer_size="5"
 work="${CONSTRAINT_TOOLS_DATA}/${work_directory}" # path to directory to store intermediate work and logs
+
+progress_bars="disk" 
+# progress_bars="stdout" 
 
 if [[ ${work_directory_should_be_clean} == "true" && -d ${work} ]]; then 
   error "the following work directory already exists:" ${work}
@@ -49,15 +51,13 @@ else
 fi 
 
 fetch_subset_of_neutral_regions () { 
+  local neutral_regions="${CONSTRAINT_TOOLS}/dist/neutral-regions-germline-grch38.bed.gz"
   less ${neutral_regions} | head -${number_of_neutral_regions}
 }
 
 train_on_subset_of_neutral_regions () {
   # file to store model in:   
-  model="${CONSTRAINT_TOOLS}/tests/germline-model/model-germline-grch38-${number_of_neutral_regions}.json" 
-
-  progress_bars="disk" 
-  # progress_bars="stdout" 
+  local model="${CONSTRAINT_TOOLS}/tests/germline-model/model-germline-grch38-${number_of_neutral_regions}.json" 
 
   # info "neutral regions (with their lengths):"
   # echo "$(fetch_subset_of_neutral_regions | awk '{ print $0, $3-$2 }')"
@@ -75,14 +75,13 @@ train_on_subset_of_neutral_regions () {
 }
 
 train_on_all_neutral_regions () {
-  model="${CONSTRAINT_TOOLS}/dist/model-germline-grch38.json" # file to store model in
+  local model="${CONSTRAINT_TOOLS}/dist/model-germline-grch38.json" # file to store model in
 
   constraint-tools train-germline-model \
     --genome ${genome} \
     --mutations ${mutations} \
     --number-chromosomes-min ${number_chromosomes_min} \
     --kmer-size ${kmer_size} \
-    --neutral-regions ${neutral_regions} \
     --model ${model} \
     --work ${work} \
     --progress-bars ${progress_bars}
