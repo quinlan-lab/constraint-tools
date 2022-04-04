fix off-by-one error in windows.py

utilities/windows.py
@@ -1,3 +1,4 @@
+from ast import Assert
 import numpy as np 
 
 # pysam API: 
@@ -5,7 +6,7 @@ import numpy as np
 import pysam
 
 from pack_unpack import unpack, pack
-from colorize import print_json
+from colorize import print_json, print_string_as_error
 
 def is_odd(filter_size): 
   if filter_size % 2 == 0: 
@@ -33,7 +34,7 @@ def create_windows(window_size, window_stride, region, genome, region_contains_w
   chromosome, region_start, region_end = unpack(region)
   window_center_start = region_start + window_size//2 if region_contains_windows else region_start
   window_center_end = region_end - window_size//2 if region_contains_windows else region_end
-  for window_center in np.arange(window_center_start, window_center_end+1, window_stride): 
+  for window_center in np.arange(window_center_start, window_center_end, window_stride): 
     # provide the "reference" argument positionally to "get_reference_length": 
     # https://stackoverflow.com/a/24463222/6674256
     window_start, window_end = compute_left_right(
@@ -49,16 +50,37 @@ def create_windows(window_size, window_stride, region, genome, region_contains_w
   return windows
 
 def test(): 
+  from colorize import print_string_as_info, print_string_as_info_dim 
+  from kmer import truncate, fetch_kmer_from_genome
   genome_filename = '/scratch/ucgd/lustre-work/quinlan/data-shared/constraint-tools/reference/grch38/hg38.analysisSet.fa.gz'
   region = 'chr1:15300-15310'
+  kmer_size = window_size = 5
   with pysam.FastaFile(genome_filename) as genome: 
-    print_json(create_windows(
-      window_size=5, 
-      window_stride=1, 
+    sequence = genome.fetch(*unpack(region))    
+    print_string_as_info(f"Sequence for region {region}:")
+    print_string_as_info_dim(truncate(sequence))
+    windows = create_windows(
+      window_size=window_size, 
+      window_stride=2,
       region=region, 
       genome=genome, 
       region_contains_windows=True
-    ))
+    )
+    print_json(windows)
+    expected_kmers = ['GGCAG', 'CAGCT', 'GCTTG']
+    for i, window in enumerate(windows): 
+      chromosome, _, _ = unpack(region)
+      kmer = fetch_kmer_from_genome(genome, chromosome, window['position'], kmer_size)
+      print(kmer)
+      try: 
+        assert kmer == expected_kmers[i]
+      except AssertionError: 
+        print_string_as_error('kmer is not expected!')
+    try: 
+      assert (i + 1) == len(expected_kmers)
+    except AssertionError: 
+      print_string_as_error('number of kmers is not the expected number!')
+    
 
 if __name__ == '__main__': 
   test()