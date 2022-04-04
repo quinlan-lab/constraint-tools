be3a4ac66a9515c0cc1383ca3cce22764dd1e8da
petermchale <peter.thomas.mchale@gmail.com>
2022-04-04 12:10:04 -0600

    implement and test fetch_kmers(..)

--- utilities/kmer.py
+++ utilities/kmer.py
@@ -1,7 +1,17 @@
 import itertools
-from colorize import print_string_as_info
+import color_traceback 
 
-from windows import is_odd, compute_left_right
+from colorize import (
+  print_string_as_error,
+  print_string_as_info,
+  print_string_as_info_dim
+)
+from windows import (
+  is_odd, 
+  compute_left_right,
+  create_windows
+)
+from pack_unpack import unpack
 
 bases = 'ACGT'
 complement = {
@@ -34,18 +44,47 @@ def contains_unspecified_bases(kmer):
   # https://www.qmul.ac.uk/sbcs/iubmb/misc/naseq.html
   return {'N', 'M', 'R'} & set(kmer)
 
-# TODO: eliminate calls to this function in favor of fetch_kmer_from_genome
-def fetch_kmer_from_sequence(sequence, position, kmer_size): 
-  left, right = compute_left_right(position, kmer_size, len(sequence))  
-  return sequence[left:right].upper()
-
-# TODO: change function signature to take a "window" object as defined in windows module 
 def fetch_kmer_from_genome(genome, chromosome, position, kmer_size): 
   # provide the "reference" argument positionally to "get_reference_length": 
   # https://stackoverflow.com/a/24463222/6674256
   left, right = compute_left_right(position, kmer_size, genome.get_reference_length(chromosome))  
   # "fetch" API: https://pysam.readthedocs.io/en/latest/api.html?highlight=fasta#pysam.FastaFile
   return genome.fetch(chromosome, left, right).upper()
+
+def truncate(string, length=100):   
+  return string[:length] + '...' if len(string) > length else string
+
+def fetch_kmers(region, genome, kmer_size): 
+  # "fetch" API: https://pysam.readthedocs.io/en/latest/api.html?highlight=fasta#pysam.FastaFile
+  # Note that fetch(region=region) does not work if the coordinates in "region" contains commas
+  # Workaround is to parse "region" into "chromosome", "start", "end": 
+  sequence = genome.fetch(*unpack(region))    
+  print_string_as_info(f"Sequence for region {region}:")
+  print_string_as_info_dim(truncate(sequence))
+
+  windows = create_windows(
+    window_size = kmer_size, 
+    window_stride = 1, 
+    region = region, 
+    genome = genome, 
+    region_contains_windows = True
+  )
+  number_of_sites = 0 
+  number_of_sites_containing_unspecified_bases = 0
+  print_string_as_info('Iterating over region {} and counting kmers...'.format(region))
+  for window in windows: 
+    number_of_sites += 1
+    chromosome, _, _ = unpack(region)
+    kmer = fetch_kmer_from_genome(genome, chromosome, window['position'], kmer_size)
+    if contains_unspecified_bases(middle_base(kmer)): 
+      number_of_sites_containing_unspecified_bases += 1
+    if contains_unspecified_bases(kmer): continue 
+    yield kmer 
+    
+  print_string_as_info_dim(
+    f'Number of sites in {region} containing unspecified bases: '
+    f'{number_of_sites_containing_unspecified_bases}/{number_of_sites}'
+  )
 
 def compute_kmers(kmer_size): 
   return [''.join(tup) for tup in itertools.product(bases, repeat=kmer_size)]
@@ -123,11 +162,34 @@ def add_kmer_counts_germline(x, y, args):
 def test_get_reverse_complement(kmer): 
   print(f"reverse complement of {kmer} is {get_reverse_complement(kmer)}")
 
-def test_get_complement(kmer): 
+def test_get_complement(kmer):
   print('')
   print_string_as_info('**** testing get_complement(ALT_state) ***** ')
   for ALT_state in compute_possible_ALT_states(kmer): 
     print(kmer, ALT_state, get_complement(ALT_state))
+
+def test_fetch_kmers(): 
+  print('')
+  print_string_as_info('**** testing fetch_kmers(...) ***** ')
+
+  import pysam 
+
+  genome_filename = '/scratch/ucgd/lustre-work/quinlan/data-shared/constraint-tools/reference/grch38/hg38.analysisSet.fa.gz'
+  region = 'chr1:15300-15310'
+  expected_kmers = ['GGCAG', 'GCAGC', 'CAGCT', 'AGCTT', 'GCTTG', 'CTTGC']
+  with pysam.FastaFile(genome_filename) as genome: 
+    number_fetched_kmers = 0
+    for i, kmer in enumerate(fetch_kmers(region, genome, kmer_size=5)): 
+      number_fetched_kmers += 1
+      print_string_as_info_dim(f'kmer: {kmer}')
+      try: 
+        assert kmer == expected_kmers[i]
+      except AssertionError: 
+        print_string_as_error('fetched kmer is not expected!')
+    try: 
+      assert number_fetched_kmers == len(expected_kmers)
+    except AssertionError: 
+      print_string_as_error('number fetched kmers differs from expected number of kmers!')
 
 if __name__ == '__main__': 
   # print(compute_kmers(3))
@@ -143,4 +205,5 @@ if __name__ == '__main__':
   test_get_reverse_complement('ACGCT')
   test_get_complement('AGTAT')
   test_get_complement('AGCGT')
+  test_fetch_kmers()
   
