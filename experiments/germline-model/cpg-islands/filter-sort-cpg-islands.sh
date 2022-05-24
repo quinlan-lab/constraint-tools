e9bb71228f5d464d3e9ae2d4ca298a30c008c612
petermchale <peter.thomas.mchale@gmail.com>
2022-05-24 10:09:36 -0600

    separate filter and sort of cpg islands

--- experiments/germline-model/cpg-islands/filter-sort-cpg-islands.sh
+++ experiments/germline-model/cpg-islands/filter-sort-cpg-islands.sh
@@ -18,18 +18,22 @@ neutral_regions=$(jq --raw-output .neutralRegions ${model})
 exons="${CONSTRAINT_TOOLS_DATA}/genes/grch38/canonical-exons.sorted.bed.gz"
 CpGs="${CONSTRAINT_TOOLS_DATA}/cpg-islands/grch38/cpg-islands.sorted.bed.gz"
 
-CpG_island_length="5"
+CpG_island_length_column="5"
 
-percentage_of_island_that_is_CpG="8"
-percentage_of_island_that_is_C_or_G="9"
-observed_CpG_number_relative_to_expectation="10"
+percentage_of_island_that_is_CpG_column="8"
+percentage_of_island_that_is_C_or_G_column="9"
+observed_CpG_number_relative_to_expectation_column="10"
+
+filter () {
+  zcat ${CpGs} |
+    awk -F $'\t' -v CpG_island_length=${CpG_island_length_column} '$CpG_island_length > 1000' |
+    bedtools intersect -a - -b ${neutral_regions} -wa -f 0.5 -u
+}
 
 filter_and_sort () {
   local sort_column=$1
-  zcat ${CpGs} |
-    awk -F $'\t' -v CpG_island_length=${CpG_island_length} '$CpG_island_length > 1000' |
-    bedtools intersect -a - -b ${neutral_regions} -wa -f 0.5 -u |
-    sort --field-separator=$'\t' -k${sort_column},${sort_column}n
+  filter | sort --field-separator=$'\t' -k${sort_column},${sort_column}n
 }
 
-filter_and_sort ${percentage_of_island_that_is_CpG}
+filter_and_sort ${percentage_of_island_that_is_CpG_column}
+
