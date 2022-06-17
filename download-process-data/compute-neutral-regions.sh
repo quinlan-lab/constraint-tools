b4c76e091ebfbbf50b0d3993a83d983b80c83562
petermchale <peter.thomas.mchale@gmail.com>
2022-06-17 17:58:32 -0600

    split promoters into train and test subsets; outline steps to train constraint model for multiple window sizes

--- download-process-data/compute-neutral-regions.sh
+++ download-process-data/compute-neutral-regions.sh
@@ -10,6 +10,7 @@ chromosome_sizes="${CONSTRAINT_TOOLS_DATA}/reference/grch38/chromosome-sizes/hg3
 
 gaps="${CONSTRAINT_TOOLS_DATA}/gaps/grch38/gaps.sorted.bed.gz"
 encode_exclude_regions="${CONSTRAINT_TOOLS_DATA}/encode-exclude-regions/grch38/encode-exclude-regions.sorted.bed.gz"
+test_promoters="${CONSTRAINT_TOOLS}/download-process-data/promoters/promoters.grch38.test.sorted.bed.gz" 
 
 # giab_difficult_regions="${CONSTRAINT_TOOLS_DATA}/giab-difficult-regions/grch38/giab-difficult-regions.sorted.bed.gz"
 # giab_low_complexity_regions="${CONSTRAINT_TOOLS_DATA}/giab-low-complexity-regions/grch38/giab-low-complexity-regions.sorted.bed.gz"
@@ -21,6 +22,6 @@ info "Compute regions putatively under neutral selection..."
 info "This is the strategy Kelley Harris’s lab took:" 
 set -o xtrace
 bedtools complement -i ${exons} -g ${chromosome_sizes} \
-  | bedtools intersect -v -a - -b ${gaps} ${encode_exclude_regions} \
+  | bedtools intersect -v -a - -b ${gaps} ${encode_exclude_regions} ${test_promoters} \
   | bedtools intersect -a - -b ${covered_sites} \
-  | sort-compress-index-bed --name ${CONSTRAINT_TOOLS}/dist/neutral-regions-germline-grch38
+  | sort-compress-index-bed --name ${CONSTRAINT_TOOLS}/dist/neutral-regions-germline-grch38-exclude-test-promoters
