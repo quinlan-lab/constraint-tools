77bc2f9d8a1a26f710b88bcd97fafacfed643442
petermchale <peter.thomas.mchale@gmail.com>
2022-07-18 10:43:25 -0600

    replace 'neutral regions' with 'trustworthy noncoding regions' in fast-train tests

--- tests/germline-model/train-germline-model.sh
+++ tests/germline-model/train-germline-model.sh
@@ -3,28 +3,28 @@ set -o pipefail
 set -o nounset
 
 number_of_jobs=$1
-number_of_neutral_regions_list=$2
-number_of_neutral_regions_list=${number_of_neutral_regions_list//,/ }
+number_of_trustworthy_noncoding_regions_list=$2
+number_of_trustworthy_noncoding_regions_list=${number_of_trustworthy_noncoding_regions_list//,/ }
 
 CONSTRAINT_TOOLS=$PWD
 PATH=${CONSTRAINT_TOOLS}/utilities:$PATH 
 
 training_durations_filename="tests/germline-model/train-durations.${number_of_jobs}-jobs.tsv"
-echo -e "number_of_neutral_regions\ttraining_duration" > ${training_durations_filename} 
+echo -e "number_of_trustworthy_noncoding_regions\ttraining_duration" > ${training_durations_filename} 
 
-for number_of_neutral_regions in $number_of_neutral_regions_list; do
-  info "Number of (unfiltered) neutral regions:" ${number_of_neutral_regions}
-  time_output_file="tests/germline-model/${number_of_neutral_regions}.time"
+for number_of_trustworthy_noncoding_regions in $number_of_trustworthy_noncoding_regions_list; do
+  info "Number of (unfiltered) trustworthy noncoding regions:" ${number_of_trustworthy_noncoding_regions}
+  time_output_file="tests/germline-model/${number_of_trustworthy_noncoding_regions}.time"
   /usr/bin/time --verbose --output ${time_output_file} bash train-germline-model.sh \
     --constraint-tools-directory ${CONSTRAINT_TOOLS} \
     --number-of-jobs ${number_of_jobs} \
-    --number-of-neutral-regions ${number_of_neutral_regions} \
-    --work-directory "work-train-germline-model-${number_of_neutral_regions}" \
+    --number-of-trustworthy-noncoding-regions ${number_of_trustworthy_noncoding_regions} \
+    --work-directory "work-train-germline-model-${number_of_trustworthy_noncoding_regions}" \
     --work-directory-should-be-clean "true"
   info "time output file:" ${time_output_file}
   training_duration=$(grep "Elapsed (wall clock) time" ${time_output_file} | awk '{ print $NF }')
   info "Training duration:" ${training_duration}
-  echo -e "${number_of_neutral_regions}\t${training_duration} (h:mm:ss or m:ss)" >> ${training_durations_filename}
+  echo -e "${number_of_trustworthy_noncoding_regions}\t${training_duration} (h:mm:ss or m:ss)" >> ${training_durations_filename}
   echo -e "\n***************************************************"
 done 
 
--- train-germline-model.sh
+++ train-germline-model.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=quinlan-shared-rw
 #SBATCH --output=dist/model-germline-grch38.log
 
-all_neutral_regions="true"
+all_trustworthy_noncoding_regions="true"
 work_directory="work-train-germline-model"
 work_directory_should_be_clean="true"
 
@@ -14,7 +14,7 @@ while [[ "$1" =~ ^- ]]; do
   case $1 in
     --constraint-tools-directory ) shift; [[ ! $1 =~ ^- ]] && CONSTRAINT_TOOLS=$1;;
     --number-of-jobs ) shift; [[ ! $1 =~ ^- ]] && number_of_jobs=$1;;
-    --number-of-neutral-regions ) shift; [[ ! $1 =~ ^- ]] && number_of_neutral_regions=$1 && all_neutral_regions="false";;
+    --number-of-trustworthy-noncoding-regions ) shift; [[ ! $1 =~ ^- ]] && number_of_trustworthy_noncoding_regions=$1 && all_trustworthy_noncoding_regions="false";;
     --work-directory ) shift; [[ ! $1 =~ ^- ]] && work_directory=$1;;
     --work-directory-should-be-clean ) shift; [[ ! $1 =~ ^- ]] && work_directory_should_be_clean=$1;;
     *) error "$0: " "$1 is an invalid flag"; exit 1;;
@@ -51,17 +51,17 @@ else
   mkdir --parents ${work}
 fi 
 
-fetch_subset_of_neutral_regions () { 
-  local neutral_regions="${CONSTRAINT_TOOLS}/dist/neutral-regions-germline-grch38.bed.gz"
-  less ${neutral_regions} | head -${number_of_neutral_regions}
+fetch_subset_of_trustworthy_noncoding_regions () { 
+  local trustworthy_noncoding_regions="${CONSTRAINT_TOOLS}/dist/trustworthy-noncoding-regions-germline-grch38-train.bed.gz"
+  less ${trustworthy_noncoding_regions} | head -${number_of_trustworthy_noncoding_regions}
 }
 
-train_on_subset_of_neutral_regions () {
+train_on_subset_of_trustworthy_noncoding_regions () {
   # file to store model in:   
-  local model="${CONSTRAINT_TOOLS}/tests/germline-model/model-germline-grch38-${number_of_neutral_regions}.json" 
+  local model="${CONSTRAINT_TOOLS}/tests/germline-model/model-germline-grch38-${number_of_trustworthy_noncoding_regions}.json" 
 
-  # info "neutral regions (with their lengths):"
-  # echo "$(fetch_subset_of_neutral_regions | awk '{ print $0, $3-$2 }')"
+  # info "trustworthy noncoding regions (with their lengths):"
+  # echo "$(fetch_subset_of_trustworthy_noncoding_regions | awk '{ print $0, $3-$2 }')"
 
   constraint-tools train-germline-model \
     --genome ${genome} \
@@ -69,14 +69,14 @@ train_on_subset_of_neutral_regions () {
     --mutations ${mutations} \
     --number-chromosomes-min ${number_chromosomes_min} \
     --kmer-size ${kmer_size} \
-    --neutral-regions <(fetch_subset_of_neutral_regions | bgzip) \
+    --trustworthy-noncoding-regions <(fetch_subset_of_trustworthy_noncoding_regions | bgzip) \
     --model ${model} \
     --work ${work} \
     --number-of-jobs ${number_of_jobs} \
     --progress-bars ${progress_bars}
 }
 
-train_on_all_neutral_regions () {
+train_on_all_trustworthy_noncoding_regions () {
   local model="${CONSTRAINT_TOOLS}/dist/model-germline-grch38.json" # file to store model in
 
   constraint-tools train-germline-model \
@@ -90,8 +90,8 @@ train_on_all_neutral_regions () {
     --progress-bars ${progress_bars}
 }
 
-if [[ ${all_neutral_regions} == "true" ]]; then
-  train_on_all_neutral_regions
+if [[ ${all_trustworthy_noncoding_regions} == "true" ]]; then
+  train_on_all_trustworthy_noncoding_regions
 else
-  train_on_subset_of_neutral_regions
+  train_on_subset_of_trustworthy_noncoding_regions
 fi 
