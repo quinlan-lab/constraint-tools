bd42b8befd69d9dd46c3f649ada4fcbc28f55c05
petermchale <peter.thomas.mchale@gmail.com>
2022-08-02 17:55:20 -0600

    download gwas catalog

---
+++ download-process-data/GWAS-catalog/download.sh
@@ -0,0 +1,5 @@
+# https://www.ebi.ac.uk/gwas/docs/faq#faq-E2
+# https://www.ebi.ac.uk/gwas/docs/file-downloads
+curl \
+  https://www.ebi.ac.uk/gwas/api/search/downloads/full \
+  > /scratch/ucgd/lustre-work/quinlan/data-shared/constraint-tools/gwas-catalog/gwas-catalog.tsv
