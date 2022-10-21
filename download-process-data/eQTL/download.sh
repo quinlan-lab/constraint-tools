460c8360e299bb693aa2ac93889ea89d0dfd9d5b
petermchale <peter.thomas.mchale@gmail.com>
2022-10-21 10:05:11 -0600

    download some eQTL data

---
+++ download-process-data/eQTL/download.sh
@@ -0,0 +1,5 @@
+# Downloaded "processed_input_data.zip" from 
+# "Data and materials availability" section of:
+# https://www.science.org/doi/10.1126/science.aaz5900
+# and extracted some data 
+# and placed at /scratch/ucgd/lustre-work/quinlan/data-shared/constraint-tools/eQTL
