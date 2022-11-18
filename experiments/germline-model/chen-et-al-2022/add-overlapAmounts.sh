7bffcb8012619046509c8af15b5a77d034c82b60
petermchale <peter.thomas.mchale@gmail.com>
2022-11-17 17:27:12 -0700

    compute overlap between windows and enhancers, exons, gaps, encode-exclude regions, uncovered-regions

--- experiments/germline-model/chen-et-al-2022/add-overlapAmounts.sh
+++ experiments/germline-model/chen-et-al-2022/add-overlapAmounts.sh
@@ -20,6 +20,11 @@ chen_mchale_windows="${CONSTRAINT_TOOLS_DATA}/benchmark-genome-wide-predictions/
 GeneHancer_enhancers="/scratch/ucgd/lustre-work/quinlan/u0055382/genome_reference/genehancer/genehancer_GRCh38.bed.gz" 
 
 merged_exons="${CONSTRAINT_TOOLS_DATA}/genes/grch38/exons.merged.bed"
+gaps="${CONSTRAINT_TOOLS_DATA}/gaps/grch38/gaps.sorted.bed.gz"
+encode_exclude_regions="${CONSTRAINT_TOOLS_DATA}/encode-exclude-regions/grch38/encode-exclude-regions.sorted.bed.gz"
+covered_regions="${CONSTRAINT_TOOLS_DATA}/gnomad/v3/gnomad_v3_coverage.filtered.sorted.bed.gz"
+
+chromosome_sizes="${CONSTRAINT_TOOLS_DATA}/reference/grch38/chromosome-sizes/hg38.chrom.sizes.sorted"
 
 get-chen-windows () { 
   cat ${chen_mchale_windows} | tail -n +2
@@ -38,9 +43,26 @@ get-merged-exons () {
   cat ${merged_exons} | uniq 
 }
 
+get-merged-gaps () {
+  bedtools merge -i ${gaps} 
+}
+
+get-merged-encode-exclude-regions () {
+  bedtools merge -i ${encode_exclude_regions} 
+}
+
+get-uncovered-regions () {
+  bedtools merge -i ${covered_regions} \
+    | bedtools complement -i - -g ${chromosome_sizes} \
+    | awk '$3 - $2 > 50'
+}
+
 # get-chen-windows | head 
 # get-GeneHancer-enhancers | head
 # get-merged-exons | head
+# get-merged-gaps | head
+# get-merged-encode-exclude-regions | head
+# get-uncovered-regions | wc -l 
 # exit 1 
 
 header-line () {
@@ -50,8 +72,18 @@ header-line () {
 add-overlapAmounts () {
   bedtools intersect \
       -a <(get-chen-windows) \
-      -b <(get-GeneHancer-enhancers) <(get-merged-exons) \
-      -names enhancer merged_exon \
+      -b \
+        <(get-GeneHancer-enhancers) \
+        <(get-merged-exons) \
+        <(get-merged-gaps) \
+        <(get-merged-encode-exclude-regions) \
+        <(get-uncovered-regions) \
+      -names \
+        enhancer \
+        merged_exon \
+        merged_gap \
+        merged_encode_exclude_region \
+        uncovered_region \
       -wao 
 }
 
