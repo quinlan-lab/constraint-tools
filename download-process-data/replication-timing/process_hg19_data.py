8bc8639118433eba6240ed452ad47544aec529f8
petermchale <peter.thomas.mchale@gmail.com>
2022-12-19 13:12:11 -0700

    compute mean replication timing per individual

--- download-process-data/replication-timing/process_hg19_data.py
+++ download-process-data/replication-timing/process_hg19_data.py
@@ -1,18 +1,27 @@
 import sys 
+import numpy as np 
 
 def process_hg19_data(): 
   header = sys.stdin.readline()   
-  replication_timing_header = '\t'.join(header.split('\t')[4:])
-  header = f'#chromosome\tstart\tend\t{replication_timing_header}'
+  header = f'#chromosome\tstart\tend\treplication timing per individual'
   header = header.strip()
   print(header)
 
   for line in sys.stdin: 
     fields = line.split('\t')
+
     region = '\t'.join(fields[:3])
     region = 'chr' + region
-    replication_timing_data = '\t'.join(fields[4:])
-    output = f'{region}\t{replication_timing_data}'
+
+    replication_timing_all_individuals = []
+    for field in fields[4:]: 
+      try: 
+        replication_timing_all_individuals.append(float(field))
+      except ValueError: 
+        continue     
+    replication_timing_per_individual = np.mean(replication_timing_all_individuals)
+
+    output = f'{region}\t{replication_timing_per_individual}'
     output = output.strip()
     print(output)
 
