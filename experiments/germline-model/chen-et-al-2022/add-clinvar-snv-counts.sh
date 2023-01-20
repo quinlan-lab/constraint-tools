7336ac0cbd73a88f6c0ff322c073f1c511cc4d19
petermchale <peter.thomas.mchale@gmail.com>
2023-01-19 17:31:23 -0700

    add ClinVar counts to each Chen window

---
+++ experiments/germline-model/chen-et-al-2022/add-clinvar-snv-counts.sh
@@ -0,0 +1,51 @@
+set -o errexit
+set -o pipefail
+# set -o noclobber
+# set -o xtrace
+set -o nounset 
+
+source set-environment-variables.sh 
+
+PATH="${CONSTRAINT_TOOLS}/experiments/germline-model/chen-et-al-2022:$PATH" 
+
+# https://stackoverflow.com/a/43476575/6674256
+# need to export PYTHONPATH since it is not already in the environment: 
+# `printenv | grep -w PYTHONPATH` returns zero output
+export PYTHONPATH="${CONSTRAINT_TOOLS}/utilities"
+                                              
+chen_mchale_windows="${CONSTRAINT_TOOLS_DATA}/benchmark-genome-wide-predictions/chen-et-al-2022/chen-mchale-quantiles.bed"
+clinvar_snvs="${CONSTRAINT_TOOLS_DATA}/clinvar-noncoding-with-negative-controls/Clinvar_nc_snv_pathogenic.hg38.bed"
+chen_mchale_windows_with_clinvar_snv_counts="${CONSTRAINT_TOOLS_DATA}/benchmark-genome-wide-predictions/chen-et-al-2022/chen-mchale-quantiles-clinvar.bed"
+
+header-line () {
+  set +o errexit
+  echo -e "$(head -1 ${chen_mchale_windows})\tClinVar SNV count"
+  set -o errexit
+}
+
+get-chen-windows () { 
+  cat ${chen_mchale_windows} | tail -n +2
+}
+
+get-clinvar-snvs () {
+  cat ${clinvar_snvs} \
+    | tail -n +2 \
+    | cut -f1-3 \
+    | sort --version-sort -k1,1 -k2,2n
+}
+
+add-clinvar-snv-counts () {
+  bedtools intersect \
+      -a <(get-chen-windows) \
+      -b <(get-clinvar-snvs) \
+      -c
+}
+
+(
+  header-line 
+  add-clinvar-snv-counts
+) > ${chen_mchale_windows_with_clinvar_snv_counts}  
+
+info "Wrote windows with clinvar snv counts to:" ${chen_mchale_windows_with_clinvar_snv_counts}  
+
+
