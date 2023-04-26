2ef0f430671bf6d2a43c578feac5367dc0297490
petermchale <peter.thomas.mchale@gmail.com>
2023-04-26 16:29:44 -0600

    save N_mean_null_X_Y[_noisy] and N_variance_null_X_Y[_noisy] to disk

--- experiments/germline-model/chen-et-al-2022/augment_header_line.noisy.py
+++ experiments/germline-model/chen-et-al-2022/augment_header_line.noisy.py
@@ -1,4 +1,4 @@
-old_header_line = 'chromosome      start   end     N_observed      N_bar_7_noncoding       N_bar_1_noncoding       N_bar_1_noncoding_noisy N_bar_3_noncoding       N_bar_3_noncoding_noisy N_bar_5_noncoding       N_bar_5_noncoding_noisy'
+old_header_line = 'chromosome      start   end     N_observed      N_bar_7_noncoding       N_mean_null_7_noncoding N_variance_null_7_noncoding     N_bar_1_noncoding       N_mean_null_1_noncoding N_variance_null_1_noncoding      N_bar_1_noncoding_noisy N_mean_null_1_noncoding_noisy   N_variance_null_1_noncoding_noisy       N_bar_3_noncoding       N_mean_null_3_noncoding N_variance_null_3_noncoding     N_bar_3_noncoding_noisy  N_mean_null_3_noncoding_noisy   N_variance_null_3_noncoding_noisy       N_bar_5_noncoding       N_mean_null_5_noncoding N_variance_null_5_noncoding     N_bar_5_noncoding_noisy N_mean_null_5_noncoding_noisy    N_variance_null_5_noncoding_noisy'
 fields = old_header_line.split()
 fields += ['feature', 'feature_chromosome', 'feature_start', 'feature_end', 'window_feature_overlap_bps']
 new_header_line = '\t'.join(fields)
