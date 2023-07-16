e8b3ddd984bae18b243b0462042ff132b82ee881
petermchale <peter.thomas.mchale@gmail.com>
2023-07-16 16:27:04 -0600

    add notes on memory requirements for nucleotide transformer

--- experiments/germline-model/chen-et-al-2022/nucleotide-transformer.2.inference.window.sh
+++ experiments/germline-model/chen-et-al-2022/nucleotide-transformer.2.inference.window.sh
@@ -4,13 +4,25 @@
 #SBATCH --partition=quinlan-rw
 #SBATCH --mem=50g # sacct -o reqmem,maxrss,averss,elapsed -j JOBID
 
+######################## [START] Notes on memory requirements #################################
+
+# "The model was trained with 8 A100 80GB" : 
+# https://huggingface.co/InstaDeepAI/nucleotide-transformer-500m-human-ref
+# Back of the envelope calculations of memory requirement of transformer models: 
+# https://twitter.com/MishaLaskin/status/1546994229674647553?s=20&t=0gkdvE1j_363D3xvTT1d4A
+# "With commonly available current hardware and model sizes, 
+# [computational and memory requirements that are quadratic with the input sequence length] 
+# typically limits the input sequence to roughly 512 tokens" : 
+# https://ai.googleblog.com/2021/03/constructing-transformers-for-longer.html
+
 # $ myallocation
 
-# Redwood general cluster hardware overview: 
+# could use as much as: 
+# --mem=192g
+# on redwood: 
 # https://www.chpc.utah.edu/documentation/guides/redwood.php#apexHardware
 
-# previously used as much as: 
-# --mem=192g
+######################## [END] Notes on memory requirements #################################
 
 # https://devhints.io/bash#miscellaneous
 # put option-fetching before "set -o nounset" so that we can detect flags without arguments
