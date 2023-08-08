1d28eef2a87b29d5bcd86c353161fc99e6f6b706
petermchale <peter.thomas.mchale@gmail.com>
2023-08-08 12:06:44 -0600

    draft code for transforming Khurana data to hg38

---
+++ download-process-data/khurana/README.md
@@ -0,0 +1,2 @@
+Used the web interface to translate hg19 coordinates of enhancers to grch38 coordinates (liftover): 
+https://genome.ucsc.edu/cgi-bin/hgLiftOver
---
+++ download-process-data/khurana/download.sh
@@ -0,0 +1,5 @@
+# https://journals.plos.org/plosgenetics/article?id=10.1371/journal.pgen.1008663#sec026
+
+directory="/scratch/ucgd/lustre-work/quinlan/data-shared/constraint-tools/khurana"
+
+curl -L https://doi.org/10.1371/journal.pgen.1008663.s010 > ${directory}/table-S2.xlsx
