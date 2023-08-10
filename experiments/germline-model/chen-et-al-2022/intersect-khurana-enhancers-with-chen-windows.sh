bcee42dc9d4cc922f8ec009e5db9742ab78cff4e
petermchale <peter.thomas.mchale@gmail.com>
2023-08-10 16:35:56 -0600

    intersect-khurana-enhancers-with-chen-windows and pull along other columns

--- experiments/germline-model/chen-et-al-2022/intersect-khurana-enhancers-with-chen-windows.sh
+++ experiments/germline-model/chen-et-al-2022/intersect-khurana-enhancers-with-chen-windows.sh
@@ -15,35 +15,25 @@ export PYTHONPATH="${CONSTRAINT_TOOLS}/utilities"
 
 get-khurana-enhancers () {
   local khurana_enhancers="${CONSTRAINT_TOOLS_DATA}/khurana/enhancers-and-khurana-scores.hg38.sorted.bed"
-  cut -f 1-3 ${khurana_enhancers} 
+  cat ${khurana_enhancers} 
 }
 
 get-chen-windows () {                   
-  cut -f 1-3 ${CONSTRAINT_TOOLS_DATA}/chen-et-al-2022-second-preprint/Supplementary_Datasets/Supplementary_Data_2.bed 
-}
-
-header-line () {
-  echo -e "chromosome_enhancer\tstart_enhancer\tend_enhancer\tchromosome_window\tstart_window\tend_window"
+  cat ${CONSTRAINT_TOOLS_DATA}/chen-et-al-2022-second-preprint/Supplementary_Datasets/Supplementary_Data_2.bed 
 }
 
 intersect-enhancers-with-windows () {
-  bedtools intersect \
-    -a <(get-khurana-enhancers) \
-    -b <(get-chen-windows) \
-    -wa \
-    -wb 
-}
-
-intersect-enhancers-with-windows-with-header () {
   khurana_enhancers_intersect_chen_windows="${CONSTRAINT_TOOLS_DATA}/khurana/khurana-enhancers-intersect-chen-windows.bed"
-  (
-    header-line 
-    intersect-enhancers-with-windows
-  ) > ${khurana_enhancers_intersect_chen_windows}  
+  bedtools intersect \
+      -a <(get-khurana-enhancers) \
+      -b <(get-chen-windows) \
+      -wa \
+      -wb \
+    > ${khurana_enhancers_intersect_chen_windows}  
   info "Wrote khurana enhancers with intersecting chen windows to:" ${khurana_enhancers_intersect_chen_windows}  
 }
 
-intersect-enhancers-with-windows-with-header
+intersect-enhancers-with-windows
 
 
 
