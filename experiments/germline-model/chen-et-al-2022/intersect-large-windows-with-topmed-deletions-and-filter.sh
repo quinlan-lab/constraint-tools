7dd10f411c4a601e638bffdb457c48663a6b0bc2
petermchale <peter.thomas.mchale@gmail.com>
2023-10-03 09:50:39 -0600

    intersect-large-windows-with-topmed-deletions-and-filter

--- experiments/germline-model/chen-et-al-2022/intersect-large-windows-with-topmed-deletions-and-filter.sh
+++ experiments/germline-model/chen-et-al-2022/intersect-large-windows-with-topmed-deletions-and-filter.sh
@@ -16,6 +16,8 @@ export PYTHONPATH="${CONSTRAINT_TOOLS}/utilities"
 LARGE_WINDOWS="${CONSTRAINT_TOOLS_DATA}/benchmark-genome-wide-predictions/chen-et-al-2022/large-windows.bed"
 TOPMED_SVS="/scratch/ucgd/lustre-work/quinlan/u0055382/SVAFotate/supporting_data/TOPMed.GRCh38.bed.gz"
 
+OVERLAP_FRACTION=$1 
+
 get-large-windows-head () {
   echo -e "chrom\tstart\tend"
 }
@@ -41,7 +43,7 @@ intersect-large-windows-with-deletions () {
   bedtools intersect \
     -a <(get-large-windows-tail) \
     -b <(get-topmed-deletions-tail) \
-    -f 0.5 \
+    -f ${OVERLAP_FRACTION} \
     -c
 }
 
@@ -59,14 +61,20 @@ get-exclude-regions () {
 }
 
 intersect-large-windows-with-deletions-and-filter () {
-  large_windows_intersect_deletions="${CONSTRAINT_TOOLS_DATA}/benchmark-genome-wide-predictions/chen-et-al-2022/large-windows-with-deletion-counts.bed"
   bedtools subtract \
     -a <(intersect-large-windows-with-deletions) \
     -b <(get-exclude-regions) \
-    -A \
-    > ${large_windows_intersect_deletions}
-  info "Wrote large windows with intersecting topmed deletions to:" ${large_windows_intersect_deletions}  
+    -A 
 }
 
-intersect-large-windows-with-deletions-and-filter
+write-large-windows-with-deletion-counts () {
+  large_windows_intersect_deletions="${CONSTRAINT_TOOLS_DATA}/benchmark-genome-wide-predictions/chen-et-al-2022/filtered-large-windows-with-deletion-counts.bed"
+  (
+    create-header
+    intersect-large-windows-with-deletions-and-filter
+  ) > ${large_windows_intersect_deletions}
+  info "Wrote (filtered) large windows with intersecting topmed deletions to:" ${large_windows_intersect_deletions}  
+}  
+
+write-large-windows-with-deletion-counts
 
