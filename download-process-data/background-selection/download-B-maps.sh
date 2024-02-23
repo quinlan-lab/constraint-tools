4291cb8c0b048c56ec92a76daaf59f2456c6add1
petermchale <peter.thomas.mchale@gmail.com>
2024-02-23 16:47:48 -0700

    convert bmaps to bed format

--- download-process-data/background-selection/download-B-maps.sh
+++ download-process-data/background-selection/download-B-maps.sh
@@ -13,15 +13,36 @@ PATH="${CONSTRAINT_TOOLS}/experiments/germline-model/chen-et-al-2022:$PATH"
 # `printenv | grep -w PYTHONPATH` returns zero output
 export PYTHONPATH="${CONSTRAINT_TOOLS}/utilities"
 
+rm -rf "${CONSTRAINT_TOOLS_DATA}/background-selection/CADD-B-map"
+
 url="https://github.com/sellalab/HumanLinkedSelectionMaps/raw/master/Bmaps/CADD_bestfit.tar.gz"
 save_path="${CONSTRAINT_TOOLS_DATA}/background-selection/CADD_bestfit.tar.gz"
 
-info "Downloading CADD bestfit data from ${url} to ${save_path}"
+info "Downloading CADD B-map from ${url} to ${save_path}"
 wget -O "${save_path}" "${url}"
 
-# Extract and uncompress the .tar.gz file
 extract_directory="${CONSTRAINT_TOOLS_DATA}/background-selection"
 tar -xzf "${save_path}" -C "${extract_directory}"
-info "Extracted CADD bestfit data to:" "${extract_directory}"
+info "Extracted CADD B-map to:" "${extract_directory}"
 rm "${save_path}"
+mv "${extract_directory}/CADD_bestfit" "${extract_directory}/CADD-B-map"
+
+# The B-maps are formatted [B, length], 
+# after McVicker et al., 2009, where B is the 
+# B value (scaled from 0-1000) covering a given 
+# chromosome segment and length is the length 
+# of the segment in base pairs. Segments sum to
+# the length of each chromosome in the hg19 
+# build.
+
+# convert B-maps to bed format: 
+for chrom_id in {1..22}; do
+  convert-bmap-to-bed \
+    < <(\
+      cat "${extract_directory}/CADD-B-map/chr${chrom_id}.bmap.txt" \
+      | awk -v id="${chrom_id}" '{print "chr"id, $1, $2}') \
+    >> "${extract_directory}/CADD-B-map/bmap.hg19.bed"
+  rm "${extract_directory}/CADD-B-map/chr${chrom_id}.bmap.txt"
+  info "Converted CADD B-map for chr${chrom_id} to bed format"
+done
 
