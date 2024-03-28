5ce60eff042e01fbdb45a34810f0d1e481baf953
petermchale <peter.thomas.mchale@gmail.com>
2024-03-28 17:42:37 -0600

    prove that gBGC coefficient was reported on hg18

--- download-process-data/download-gBGC-coefficient.sh
+++ download-process-data/download-gBGC-coefficient.sh
@@ -20,6 +20,12 @@ info "download directory is:" ${gBGC_path}
 
 # https://genome.cshlp.org/content/25/8/1215/suppl/DC1
 # https://genome.cshlp.org/content/suppl/2015/06/03/gr.185488.114.DC1/ReadMe.txt
+# Text S5 shows that coordinate system is hg18: 
+# "As a control, we also used the pedigree-based genetic map from deCODE (Kong et al. 2010). 
+#  As these maps are reported in the version hg18 of the human genome assembly, 
+#  we converted the location of SNPs from hg19 to hg18 coordinates using the liftOver tool 
+#  (https://genome.ucsc.edu/cgi-bin/hgLiftOver). 
+#  A small fraction of SNPs (N=31,056) could not be mapped onto hg18 and the SNPs were discarded."
 curl "https://genome.cshlp.org/content/suppl/2015/06/03/gr.185488.114.DC1/B_estimates_${pop}.txt" \
   | tr '\r' '\n' \
   | process_gBGC_coefficient_data \
