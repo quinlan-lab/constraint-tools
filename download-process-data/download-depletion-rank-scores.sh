272e541835ae72d0191d163cb87f3424043c7e81
petermchale <peter.thomas.mchale@gmail.com>
2024-06-25 16:37:39 -0600

    specify genome reference

--- download-process-data/download-depletion-rank-scores.sh
+++ download-process-data/download-depletion-rank-scores.sh
@@ -11,6 +11,9 @@ source download-process-data/set-environment-variables.sh
 PATH="${CONSTRAINT_TOOLS}/bin:$PATH" 
 PATH="${CONSTRAINT_TOOLS}/utilities:$PATH" 
 
+# https://www.nature.com/articles/s41586-022-04965-x
+# GRCH38
+
 # info "Downloading..." 
 # curl https://static-content.springer.com/esm/art%3A10.1038%2Fs41586-022-04965-x/MediaObjects/41586_2022_4965_MOESM3_ESM.gz \
 #   > ${CONSTRAINT_TOOLS_DATA}/depletion_rank_scores/41586_2022_4965_MOESM3_ESM.gz
