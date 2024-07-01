9cfd25e336e754edb8682ffc0fbb9fd0c6137623
petermchale <peter.thomas.mchale@gmail.com>
2024-07-01 16:46:36 -0600

    add header line to BGS map

--- download-process-data/background-selection/download-B-maps.sh
+++ download-process-data/background-selection/download-B-maps.sh
@@ -4,7 +4,7 @@ set -o pipefail
 # set -o xtrace
 set -o nounset
 
-source /scratch/ucgd/lustre-work/quinlan/u6018199/constraint-tools/set-environment-variables.sh 
+source /scratch/ucgd/lustre-labs/quinlan/u6018199/constraint-tools/set-environment-variables.sh 
 
 PATH="${CONSTRAINT_TOOLS}/experiments/germline-model/chen-et-al-2022:$PATH"
 
@@ -13,40 +13,42 @@ PATH="${CONSTRAINT_TOOLS}/experiments/germline-model/chen-et-al-2022:$PATH"
 # `printenv | grep -w PYTHONPATH` returns zero output
 export PYTHONPATH="${CONSTRAINT_TOOLS}/utilities"
 
-extract_directory="${CONSTRAINT_TOOLS_DATA}/background-selection"
+EXTRACT_DIRECTORY="${CONSTRAINT_TOOLS_DATA}/background-selection"
 
-rm -rf "${CONSTRAINT_TOOLS_DATA}/background-selection/CADD-B-map"
+download_and_preprocess () {
+  rm -rf "${CONSTRAINT_TOOLS_DATA}/background-selection/CADD-B-map"
 
-url="https://github.com/sellalab/HumanLinkedSelectionMaps/raw/master/Bmaps/CADD_bestfit.tar.gz"
-save_path="${CONSTRAINT_TOOLS_DATA}/background-selection/CADD_bestfit.tar.gz"
+  url="https://github.com/sellalab/HumanLinkedSelectionMaps/raw/master/Bmaps/CADD_bestfit.tar.gz"
+  save_path="${CONSTRAINT_TOOLS_DATA}/background-selection/CADD_bestfit.tar.gz"
 
-info "Downloading CADD B-map from ${url} to ${save_path}"
-wget -O "${save_path}" "${url}"
+  info "Downloading CADD B-map from ${url} to ${save_path}"
+  wget -O "${save_path}" "${url}"
 
-tar -xzf "${save_path}" -C "${extract_directory}"
-info "Extracted CADD B-map to:" "${extract_directory}"
-rm "${save_path}"
-mv "${extract_directory}/CADD_bestfit" "${extract_directory}/CADD-B-map"
+  tar -xzf "${save_path}" -C "${EXTRACT_DIRECTORY}"
+  info "Extracted CADD B-map to:" "${EXTRACT_DIRECTORY}"
+  rm "${save_path}"
+  mv "${EXTRACT_DIRECTORY}/CADD_bestfit" "${EXTRACT_DIRECTORY}/CADD-B-map"
 
-# The B-maps are formatted [B, length], 
-# after McVicker et al., 2009, where B is the 
-# B value (scaled from 0-1000) covering a given 
-# chromosome segment and length is the length 
-# of the segment in base pairs. Segments sum to
-# the length of each chromosome in the hg19 
-# build.
-for chrom_id in {1..22}; do
-  convert-bmap-to-bed \
-    < <(\
-      cat "${extract_directory}/CADD-B-map/chr${chrom_id}.bmap.txt" \
-      | awk -v id="${chrom_id}" '{print "chr"id, $1, $2}') \
-    >> "${extract_directory}/CADD-B-map/bmap.hg19.bed"
-  rm "${extract_directory}/CADD-B-map/chr${chrom_id}.bmap.txt"
-  info "Converted CADD B-map for chr${chrom_id} to bed format"
-done
+  # The B-maps are formatted [B, length], 
+  # after McVicker et al., 2009, where B is the 
+  # B value (scaled from 0-1000) covering a given 
+  # chromosome segment and length is the length 
+  # of the segment in base pairs. Segments sum to
+  # the length of each chromosome in the hg19 
+  # build.
+  for chrom_id in {1..22}; do
+    convert-bmap-to-bed \
+      < <(\
+        cat "${EXTRACT_DIRECTORY}/CADD-B-map/chr${chrom_id}.bmap.txt" \
+        | awk -v id="${chrom_id}" '{print "chr"id, $1, $2}') \
+      >> "${EXTRACT_DIRECTORY}/CADD-B-map/bmap.hg19.bed"
+    rm "${EXTRACT_DIRECTORY}/CADD-B-map/chr${chrom_id}.bmap.txt"
+    info "Converted CADD B-map for chr${chrom_id} to bed format"
+  done
 
-sort -k1,1V -k2,2n "${extract_directory}/CADD-B-map/bmap.hg19.bed" > "${extract_directory}/CADD-B-map/bmap.hg19.sorted.bed"
-rm "${extract_directory}/CADD-B-map/bmap.hg19.bed"
+  sort -k1,1V -k2,2n "${EXTRACT_DIRECTORY}/CADD-B-map/bmap.hg19.bed" > "${EXTRACT_DIRECTORY}/CADD-B-map/bmap.hg19.sorted.bed"
+  rm "${EXTRACT_DIRECTORY}/CADD-B-map/bmap.hg19.bed"
+}
 
 lift () {
   local filename_root=$1
@@ -64,7 +66,16 @@ lift () {
     ${filename_root}.hg19.unmapped.bed
 }
 
-lift "${extract_directory}/CADD-B-map/bmap"
+add_header_line () {
+  echo -e "chromosome\tstart\tend\tB" \
+    | cat - "${EXTRACT_DIRECTORY}/CADD-B-map/bmap.hg38.bed" \
+    > "${EXTRACT_DIRECTORY}/CADD-B-map/bmap.hg38.header.bed"
+}
+
+# download_and_preprocess 
+# lift "${EXTRACT_DIRECTORY}/CADD-B-map/bmap"
+add_header_line
+
 
 
 
