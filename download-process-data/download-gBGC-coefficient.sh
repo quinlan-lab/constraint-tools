0b16ea65c8d79f705d426b69f3a74480342b0b4a
petermchale <peter.thomas.mchale@gmail.com>
2024-07-02 15:25:26 -0600

    add header line to gBGC map

--- download-process-data/download-gBGC-coefficient.sh
+++ download-process-data/download-gBGC-coefficient.sh
@@ -12,42 +12,50 @@ PATH="${CONSTRAINT_TOOLS}/bin:$PATH"
 PATH="${CONSTRAINT_TOOLS}/utilities:$PATH" 
 
 # exporting these variables make them visible in python scripts below 
-export gBGC_path="${CONSTRAINT_TOOLS_DATA}/GC-biased-gene-conversion"
-export pop="EUR"
+export GBGC_PATH="${CONSTRAINT_TOOLS_DATA}/GC-biased-gene-conversion"
+export POP="EUR"
 
-mkdir --parents ${gBGC_path}
-info "download directory is:" ${gBGC_path}
+mkdir --parents ${GBGC_PATH}
+info "download directory is:" ${GBGC_PATH}
 
-# https://genome.cshlp.org/content/25/8/1215/suppl/DC1
-# https://genome.cshlp.org/content/suppl/2015/06/03/gr.185488.114.DC1/ReadMe.txt
-# Text S5 shows that coordinate system is hg18: 
-# "As a control, we also used the pedigree-based genetic map from deCODE (Kong et al. 2010). 
-#  As these maps are reported in the version hg18 of the human genome assembly, 
-#  we converted the location of SNPs from hg19 to hg18 coordinates using the liftOver tool 
-#  (https://genome.ucsc.edu/cgi-bin/hgLiftOver). 
-#  A small fraction of SNPs (N=31,056) could not be mapped onto hg18 and the SNPs were discarded."
-curl "https://genome.cshlp.org/content/suppl/2015/06/03/gr.185488.114.DC1/B_estimates_${pop}.txt" \
-  | tr '\r' '\n' \
-  | process_gBGC_coefficient_data \
-  > ${gBGC_path}/gBGC-coefficient.hg18.${pop}.tsv
-
-prepare_for_liftover 
+download () { 
+  # https://genome.cshlp.org/content/25/8/1215/suppl/DC1
+  # https://genome.cshlp.org/content/suppl/2015/06/03/gr.185488.114.DC1/ReadMe.txt
+  # Text S5 shows that coordinate system is hg18: 
+  # "As a control, we also used the pedigree-based genetic map from deCODE (Kong et al. 2010). 
+  #  As these maps are reported in the version hg18 of the human genome assembly, 
+  #  we converted the location of SNPs from hg19 to hg18 coordinates using the liftOver tool 
+  #  (https://genome.ucsc.edu/cgi-bin/hgLiftOver). 
+  #  A small fraction of SNPs (N=31,056) could not be mapped onto hg18 and the SNPs were discarded."
+  curl "https://genome.cshlp.org/content/suppl/2015/06/03/gr.185488.114.DC1/B_estimates_${POP}.txt" \
+    | tr '\r' '\n' \
+    | process_gBGC_coefficient_data \
+    > ${GBGC_PATH}/gBGC-coefficient.hg18.${POP}.tsv
+}
 
 lift () {
   info "Lifting over..."
   bash \
     ${CONSTRAINT_TOOLS}/download-process-data/lift.sh \
-    "${gBGC_path}/gBGC-coefficient.hg18.${pop}.bed" \
+    "${GBGC_PATH}/gBGC-coefficient.hg18.${POP}.bed" \
     hg18 \
     hg38 \
     "-bedPlus=3 -tab"
   mv \
-    "${gBGC_path}/gBGC-coefficient.hg18.${pop}.bed.hg38" \
-    "${gBGC_path}/gBGC-coefficient.hg38.${pop}.bed"
+    "${GBGC_PATH}/gBGC-coefficient.hg18.${POP}.bed.hg38" \
+    "${GBGC_PATH}/gBGC-coefficient.hg38.${POP}.bed"
   mv \
-    "${gBGC_path}/gBGC-coefficient.hg18.${pop}.bed.hg38.unmapped" \
-    "${gBGC_path}/gBGC-coefficient.hg18.${pop}.umapped.bed"
+    "${GBGC_PATH}/gBGC-coefficient.hg18.${POP}.bed.hg38.unmapped" \
+    "${GBGC_PATH}/gBGC-coefficient.hg18.${POP}.umapped.bed"
 }
 
-lift 
+add_header_line () {
+  print_header_line \
+    | cat - "${GBGC_PATH}/gBGC-coefficient.hg38.${POP}.bed" \
+    > "${GBGC_PATH}/gBGC-coefficient.hg38.${POP}.header.bed"
+}
 
+# download
+# prepare_for_liftover 
+# lift 
+add_header_line
