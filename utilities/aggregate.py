c33f1439cc7c85f4e4028c412ed2b72a0418bbed
petermchale <peter.thomas.mchale@gmail.com>
2024-07-02 17:23:47 -0600

    implement a polars aggregate library routine

--- utilities/aggregate.py
+++ utilities/aggregate.py
@@ -5,3 +5,11 @@ def aggregate(df, group_columns, aggregation_functions):
   df.columns = [' '.join(col[::-1]).strip() for col in df.columns.values]
   return df
 
+def aggregate_polars(df, group_columns, aggregation_functions): 
+    df_grouped = ( 
+        df
+        .group_by(group_columns, maintain_order=True)
+        .agg(*aggregation_functions)
+    )
+    return df_grouped
+  
